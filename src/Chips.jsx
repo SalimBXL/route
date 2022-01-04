9cf48b4fb349456b6cf2ea260cac8af92c45c5f3
Salim A. JOLY <el.salim.salim@gmail.com>
2022-01-04 11:13:55 +0100

    ok

--- src/Chips.jsx
+++ src/Chips.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 import FrameBox from "./FrameBox";
 import { Link } from "react-router-dom";
+import "./Chips.css";
 
 const Chips = () => {
     return (
-        <div>
+        <div className="Chips">
             <FrameBox>
                 <h1>Chips</h1>
             </FrameBox>
--- src/Soda.jsx
+++ src/Soda.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 import FrameBox from "./FrameBox";
 import { Link } from "react-router-dom";
+import "./Soda.css";
 
 const Soda = ({name}) => {
     return (
-        <div>
+        <div className="Soda">
             <FrameBox>
                 <h1>Soda</h1>
             </FrameBox>
